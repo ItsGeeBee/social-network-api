e563817cfe6af42c3d20770079a3f281e38b4b10
ItsGeeBee <gracelbrown@hotmail.com>
2022-09-15 12:28:30 +1000

    Started thought controllers

---
+++ controllers/thought-contoller.js
@@ -0,0 +1,39 @@
+const { User, Thought} = require('../models');
+
+const thoughtController = {
+  getAllThought(req, res) {
+    Thought.find({})
+      .populate({
+        path: 'reactions',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // get one thoughts by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: 'reactions',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thoughts found with that id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -67,4 +67,7 @@ const userController = {
         res.json(dbUserData);
       })
       .catch((err) => res.status(400).json(err));
-  },
+  }};
+
+  
+  module.exports = userController
